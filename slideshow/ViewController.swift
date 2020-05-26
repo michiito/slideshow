f76f5f60402d3f92aa4a59acb63ec88e35894be7
michi <michi1008_ito@yahoo.co.jp>
2020-05-26 20:55:19 +0900

    Initial Commit

---
+++ slideshow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  slideshow
+//
+//  Created by 伊藤倫 on 2020/05/26.
+//  Copyright © 2020 michi.ito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
